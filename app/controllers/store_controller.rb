a10f9a549e43c40c5244ec04526ab0834e3e8d70
Slava Rybalka <slavarybalko@gmail.com>
2015-09-17 21:00:53 -0500

    Commit for Miguel, this commit returns an error

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+
   def index
+  	
   	@products = Product.order(:title)
+  	@counter = session[:counter]
+  	if session[:counter].nil?
+  	  @counter = 1
+  	else
+  	  @counter +=1
+  	session[:counter] = @counter
+  	end	
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :title, length: { minimum: 10 }
@@ -11,6 +15,18 @@ class Product < ActiveRecord::Base
     def self.latest
 	Product.order(:updated_at).last
     end
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end  
+
+
 end
 
 
